a84f56042fe4bb1861f220233199b1d468c029cb
yasudanaoya <tamago.708@gmail.com>
2022-07-18 17:13:21 +0900

    finish 2.7

--- quickreplace/src/main.rs
+++ quickreplace/src/main.rs
@@ -1,10 +1,48 @@
-use std::env;
+use std::{env, fs};
 
+use regex::Regex;
 use text_colorizer::*;
 
 fn main() {
     let args = parse_args();
-    println!("{:?}", args);
+
+    let data = match fs::read_to_string(&args.filename) {
+        Ok(v) => v,
+        Err(e) => {
+            eprintln!(
+                "{} failed to read from file '{}': {:?}",
+                "Error:".red().bold(),
+                args.filename,
+                e,
+            );
+            std::process::exit(1);
+        }
+    };
+
+    let replaced_data = match replace(&args.target, &args.replacement, &data) {
+        Ok(v) => v,
+        Err(e) => {
+            eprintln!(
+                "{} failed to replace in file '{}': {:?}",
+                "Error:".red().bold(),
+                args.filename,
+                e,
+            );
+            std::process::exit(1);
+        }
+    };
+
+    match fs::write(&args.output, &replaced_data) {
+        Ok(_) => {}
+        Err(e) => {
+            eprintln!(
+                "{} failed to write to file '{}': {:?}",
+                "Error:".red().bold(),
+                args.filename,
+                e,
+            )
+        }
+    };
 }
 
 #[derive(Debug)]
@@ -42,4 +80,9 @@ fn parse_args() -> Arguments {
         filename: args[2].clone(),
         output: args[3].clone(),
     }
+}
+
+fn replace(target: &str, replacement: &str, text: &str) -> Result<String, regex::Error> {
+    let regex = Regex::new(target)?;
+    Ok(regex.replace_all(text, replacement).to_string())
 }
